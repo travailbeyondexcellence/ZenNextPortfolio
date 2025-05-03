e56e74425d0f0311080ac4ab7ff72849b00a63e1
travailbeyondexcellence <zen@paathshala.world>
2025-05-03 11:10:36 +0530

    Implement code changes to enhance functionality and improve performance

--- src/app/data.js
+++ src/app/data.js
@@ -96,22 +96,22 @@ export const BtnList = [
   { label: "Contact", link: "/contact", icon: "contact", newTab: false },
   {
     label: "Github",
-    link: "https://www.github.com/codebucks27",
+    link: "https://www.github.com/travailbeyondexcellence",
     icon: "github",
     newTab: true,
   },
   {
     label: "LinkedIn",
-    link: "https://www.linkedin.com/in/codebucks",
+    link: "https://www.linkedin.com/in/zenith-physics",
     icon: "linkedin",
     newTab: true,
   },
-  {
-    label: "X",
-    link: "https://www.x.com/code_bucks",
-    icon: "twitter",
-    newTab: true,
-  },
+  // {
+  //   label: "X",
+  //   link: "https://www.x.com/code_bucks",
+  //   icon: "twitter",
+  //   newTab: true,
+  // },
   {
     label: "Resume",
     link: "/resume.pdf",
